4799461f9509eaa154e1b997dca7592a425ab36a
Simon Ma <chnmayu@gmail.com>
2024-04-10 21:54:11 +0800

    11

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,7 @@ const express = require("express");
 const proxy = require('express-http-proxy');
 const app = express();
 
-app.get("/test", (req, res) => res.send("Express on Vercel"));
-app.use('/api', proxy('https://home.simonmy.com:20443/api'));
+app.use('/', proxy('https://home.simonmy.com:20443/api'));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
