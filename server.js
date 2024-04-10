15786e85ee129d3d691c06111bb423a5de1ac483
Simon Ma <chnmayu@gmail.com>
2024-04-10 20:39:47 +0800

    add config

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+var express = require('../..');
+var logger = require('morgan');
+var path = require('path');
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
