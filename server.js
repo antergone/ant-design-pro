721ee10f7b53f0bd333cf286fce0ef8582b9c0c5
Simon Ma <chnmayu@gmail.com>
2024-04-10 20:42:30 +0800

    add logger

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var logger = require('morgan');
 var path = require('path');
 var app = express();
 
+app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(3000);
