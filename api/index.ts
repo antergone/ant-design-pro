69f9e94bbf336068c5e52d42dfa3c48660192493
Simon Ma <chnmayu@gmail.com>
2024-04-10 21:03:54 +0800

    add proxy

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,20 @@
 const express = require("express");
+const http_proxy = require('http-proxy-middleware');
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const proxy = {
+  '/api/': {
+    target: 'https://home.simonmy.com:20443/api/'
+  },
+
+};
+
+for (let key in proxy) {
+  app.use(key, http_proxy(proxy[key]));
+}
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
